5445008c63e7446a6588faf0fc3f98f02effd4be
codaMW <codamw011@gmail.com>
2025-03-21 17:32:54 +0000

    03.sh

--- submission/03.sh
+++ submission/03.sh
@@ -1,3 +1,10 @@
 # Created a SegWit address.
 # Add funds to the address.
 # Return only the Address
+
+GWIT=$(bitcoin-cli -regtest getnewaddress "" bech32)
+
+bitcoin-cli -regtest generatetoaddress 101 $SEGWIT
+
+echo $SEGWIT
+
