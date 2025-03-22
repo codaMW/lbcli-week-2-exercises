fde87cfefe6d4204c7e75b6552bba8a3b0a71fcc
codaMW <codamw011@gmail.com>
2025-03-22 16:54:57 +0000

    03.sh ..

--- submission/03.sh
+++ submission/03.sh
@@ -2,9 +2,10 @@
 # Add funds to the address.
 # Return only the Address
 
+
 SEGWIT=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress "" "bech32")
 
-bitcoin-cli -regtest generatetoaddress 101 "$SEGWIT"
+bitcoin-cli -regtest -rpcwallet=btrustwallet generatetoaddress 101 "$SEGWIT"
 
 echo "$SEGWIT"
 
