c87576b8c54434b80ed34f86b259b12ebc060aa6
codaMW <codamw011@gmail.com>
2025-03-24 15:08:58 +0000

    believe me

--- submission/03.sh
+++ submission/03.sh
@@ -3,13 +3,15 @@
 # Return only the Address
 
 
+# Generate a new SegWit address (Bech32 format)
+SEGWIT=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress "" bech32)
 
-# Generate a new SegWit address
-SEGWIT=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress "bech32")
-
-# Mine 101 blocks to the address
+# Mine 101 blocks to the generated address
 bitcoin-cli -regtest generatetoaddress 101 "$SEGWIT"
 
-# Print only the SegWit address
-echo "$SEGWIT"
+# Fetch and return only the address from the UTXO set
+Returned_addr=$(bitcoin-cli -regtest -rpcwallet=btrustwallet listunspent | jq -r '.[0].address')
+
+# Print the returned address
+echo "$Returned_addr"
 
