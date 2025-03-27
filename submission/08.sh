f5f94b8dd01acdc1125b98e827b147c8998cbe87
codaMW <codamw011@gmail.com>
2025-03-27 06:32:39 +0000

    match check

--- submission/08.sh
+++ submission/08.sh
@@ -4,45 +4,18 @@
 # Use the UTXOs from the transaction below
 raw_tx="01000000000101c8b0928edebbec5e698d5f86d0474595d9f6a5b2e4e3772cd9d1005f23bdef772500000000ffffffff0276b4fa0000000000160014f848fe5267491a8a5d32423de4b0a24d1065c6030e9c6e000000000016001434d14a23d2ba08d3e3edee9172f0c97f046266fb0247304402205fee57960883f6d69acf283192785f1147a3e11b97cf01a210cf7e9916500c040220483de1c51af5027440565caead6c1064bac92cb477b536e060f004c733c45128012102d12b6b907c5a1ef025d0924a29e354f6d7b1b11b5a7ddff94710d6f0042f3da800000000"
 
-
-
-#txn_id=$(bitcoin-cli -regtest decoderawtransaction $raw_tx | jq -r '.txid')
-
-
-#utxo_vout_1=$(bitcoin-cli -regtest decoderawtransaction $raw_tx | jq -r '.vout | .[0] | .n // .vout')
-#utxo_vout_1_value=$(bitcoin-cli -regtest decoderawtransaction $raw_tx | jq -r '.vout | .[0] | .value')
-
-
-#recipient="2MvLcssW49n9atmksjwg2ZCMsEMsoj3pzUP"
-
-#utxo_vout_2=$(bitcoin-cli -regtest decoderawtransaction $raw_tx | jq -r '.vout | .[1] | .n // .vout')
-#utxo_vout_2_value=$(bitcoin-cli -regtest decoderawtransaction $raw_tx | jq -r '.vout | .[1] | .value')
-
-
-#new_raw_tx_hex=$(bitcoin-cli -regtest -named createrawtransaction inputs="[ { \"txid\": \"$txn_id\", \"vout\": $utxo_vout_1 }, { \"txid\": \"$txn_id\", \"vout\": $utxo_vout_2, \"sequence\": 1 } ]" outputs="{ \"$recipient\": 0.20000000 }")
-
-#echo "$new_raw_tx_hex"
-
-
-#!/bin/bash
-
-
 # Extract transaction details
 txid=$(bitcoin-cli -regtest decoderawtransaction "$raw_tx" | jq -r '.txid')
-
-# Get UTXO details (vout indices and values)
 vout1=$(bitcoin-cli -regtest decoderawtransaction "$raw_tx" | jq -r '.vout[0].n')
 vout2=$(bitcoin-cli -regtest decoderawtransaction "$raw_tx" | jq -r '.vout[1].n')
-vout1_value=$(bitcoin-cli -regtest decoderawtransaction "$raw_tx" | jq -r '.vout[0].value')
-vout2_value=$(bitcoin-cli -regtest decoderawtransaction "$raw_tx" | jq -r '.vout[1].value')
 
-# Recipient address
 recipient="2MvLcssW49n9atmksjwg2ZCMsEMsoj3pzUP"
 
-# Create raw transaction with RBF enabled (sequence=1 for the second input)
+# Create raw transaction with:
+# - First input: sequence=0 (explicitly disable RBF)
+# - Second input: sequence=1 (enable RBF)
 raw_tx_hex=$(bitcoin-cli -regtest createrawtransaction \
-  '[{"txid":"'$txid'","vout":'$vout1'},{"txid":"'$txid'","vout":'$vout2',"sequence":1}]' \
+  '[{"txid":"'$txid'","vout":'$vout1',"sequence":0},{"txid":"'$txid'","vout":'$vout2',"sequence":1}]' \
   '{"'$recipient'":0.20000000}')
 
 echo "$raw_tx_hex"
-
